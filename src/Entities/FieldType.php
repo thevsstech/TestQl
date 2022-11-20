654785767cfa3ccf2a5fb1f074b7b75c04c4e359
vahit sağlam <vahit.serif119@gmail.com>
2022-11-20 04:17:33 +0300

    Added new field types

--- src/Entities/FieldType.php
+++ src/Entities/FieldType.php
@@ -12,4 +12,10 @@ enum FieldType
     case OBJECT;
     case ARRAY;
     case NULL;
+    case ARRAY_OF_STRINGS;
+    case ARRAY_OF_OBJECTS;
+    case ARRAY_OF_BOOLEANS;
+    case ARRAY_OF_INTEGERS;
+    case ARRAY_OF_FLOATS;
+    case ARRAY_OF_DATES;
 }
